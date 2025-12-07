bbf1c34c0174d700cb2ffe52283ac410e6db3492
Datom-code <enochgbadebo007@gmail.com>
2025-12-07 20:20:36 +0100

    first commit

---
+++ authentication_module/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+# Create your models here.
+#Job Application Model
+
+
+# class JobApplication(models.Model):
---
+++ authentication_module/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
